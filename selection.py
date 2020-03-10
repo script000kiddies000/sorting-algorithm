e213701172702418825e2f77829f58476bf4f670
Your Name <scriptkiddies@protonmail.com>
2020-03-10 23:49:10 +0700

    add selection sort python

---
+++ selection.py
@@ -0,0 +1,26 @@
+A = [75, 25,66.1,103,68,77,43,17,4, 102, 22, 11] 
+
+def swap(a,b):
+  swap = a
+  a = b
+  b = swap
+  return a,b
+
+print("array awal = ",A)
+
+# Traverse through all array elements 
+for i in range(len(A)): 
+    print("iterasi ke ",i,A)
+    # Find the minimum element in remaining  
+    # unsorted array 
+    min_idx = i 
+    for j in range(i+1, len(A)): 
+        if A[min_idx] > A[j]: 
+            min_idx = j 
+              
+    # Swap the found minimum element with  
+    # the first element[i], A[min_idx]         
+    #A[i], A[min_idx] = A[min_idx], A[i] 
+    A[i], A[min_idx] = swap( A[i], A[min_idx])
+
+print("hasil = ",A)
