bab7ba2ec06c38cf9d495806c5b0fa29f37f73f2
script000kiddies000 <55286658+script000kiddies000@users.noreply.github.com>
2020-03-24 21:52:55 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import buble, selection, time
+
+data = [10, 2, 6, 7, 1, 3, 15] 
+
+def cekWaktu(start, end):
+  return "Selesai dalam %f detik\n" % (end - start)
+
+while True:
+  jwb = input("1. buble sort \n2. selection sort \npilih yang mana ??  ")
+
+  if jwb == '1' :
+    start = time.time()
+    buble.bubbleSort(data)
+    end = time.time()
+    print(cekWaktu(start, end))
+
+  elif jwb == '2' :
+    start = time.time()
+    # buble.bubbleSort(data)
+    selection.selection(data)
+    end = time.time()
+    print(cekWaktu(start, end))   
+
+  else:
+    exit()
+
+  
+
